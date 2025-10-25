705c8f148e858953ea9138c82339df56250e6f63
Kamil <kamilbuszta.3@gmail.com>
2025-10-25 21:53:21 +0200

    C 1_2.1

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 (function () {
-  //TODO
+  //C 1_2.1
+    var ex3_element = document.getElementById('ex3_element');
+    ex3_element.setAttribute('draggable','true');
+    var ex3_two = document.getElementById("ex3_two");
+    ex3_two.addEventListener("dragover", (event) =>{
+      event.preventDefault();
+    })
+    ex3_two.addEventListener("drop",(event)=>{
+      event.preventDefault();
+      ex3_two.appendChild(ex3_element);
+      ex3_element.draggable = false;
+    })
 })();
